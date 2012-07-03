bd9dcf58ef19f72ff47fe26f45942ca916e353cd
caarlos0 <caarlos0@gmail.com>
2012-07-03 19:18:27 -0300

    merge..

--- src/main/java/com/github/caarlos0/Start.java
+++ src/main/java/com/github/caarlos0/Start.java
@@ -1,16 +1,16 @@
 package com.github.caarlos0;
 
-import com.github.caarlos0.dao.AbstractDao;
+import com.github.caarlos0.dao.GenericDao;
 import com.github.caarlos0.model.Pessoa;
 
 import javax.inject.Inject;
 
 public class Start {
 
-    private final AbstractDao<Pessoa> dao;
+    private final GenericDao<Pessoa> dao;
 
     @Inject
-    public Start(AbstractDao<Pessoa> dao) {
+    public Start(GenericDao<Pessoa> dao) {
         this.dao = dao;
 
         System.out.println((dao != null) + " maoee");
--- src/main/java/com/github/caarlos0/dao/GenericDao.java
+++ src/main/java/com/github/caarlos0/dao/GenericDao.java
@@ -1,12 +1,66 @@
 package com.github.caarlos0.dao;
 
-
 import com.github.caarlos0.model.Bean;
+import com.google.inject.Provider;
+import com.google.inject.persist.Transactional;
 
-public class GenericDao<T extends Bean> extends AbstractDao<T> {
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
+import java.util.List;
 
-    @Override
-    protected Class<T> clazz() {
-        return null;
+/**
+ * Create your specific DAO extending this class, override the constructor ths way:
+ * 
+ * <code>
+ * public class CursoDao extends GenericDao<Foo> {
+ *   @Inject
+ *   public CursoDao(Provider<EntityManager> emf) {
+ *       super(emf, Foo.class);
+ *   }
+ * }
+ * </code>
+ * 
+ * In this case, <i>Foo</i> has to extend <code>Bean</code>.
+ *
+ * @author Carlos A Becker
+ */
+public abstract class GenericDao<T extends Bean> {
+   
+
+    @Inject
+    private Provider<EntityManager> emp;
+
+    protected EntityManager em() {
+        return emp.get();
+    }
+
+    protected abstract Class<T> clazz();
+
+    @Inject
+    public GenericDao() {
+    }
+
+    @Transactional
+    public void save(T t) {
+        if(t.getId() != null) {
+            t = em().merge(t);
+        } else {
+            em().persist(t);
+        }
+    }
+    
+    public List<T> findAll() {
+        TypedQuery<T> q = em().createQuery("select e from " + clazz().getSimpleName() + " e", clazz());
+        return q.getResultList();
+    }
+    
+    public T find(Long id) {
+        return em().getReference(clazz(), id);
+    }
+    
+    @Transactional
+    public void remove(T t) {
+        em().remove(t);
     }
 }
